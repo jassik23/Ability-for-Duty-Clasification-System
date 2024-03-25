e16400b7afeede5861d5b99c41251dcaa8d3a288
XoTTaB <jassik2002@mail.ru>
2024-03-25 23:44:08 +1000

    Create: Создание интерфейса системы

---
+++ Ability-for-Duty-Clasification-System/BaseOfKnowlengeEditor.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Windows;
+
+namespace Ability_for_Duty_Clasification_System;
+
+public partial class BaseOfKnowlengeEditor : Window
+{
+    public BaseOfKnowlengeEditor()
+    {
+        InitializeComponent();
+    }
+    private void ClassButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        ClassEditor newWindow = new ClassEditor();
+        newWindow.Show();
+        this.Close();
+    }
+
+    private void EditMarks_OnClick(object sender, RoutedEventArgs e)
+    {
+        CharacteristicOfClassEditor newWindow = new CharacteristicOfClassEditor();
+        newWindow.Show();
+        this.Close();
+    }
+
+    private void CheckBaseOfKnowlenge_OnClick(object sender, RoutedEventArgs e)
+    {
+        CheckClassConsistency newWindow = new CheckClassConsistency();
+        newWindow.Show();
+        this.Close();
+    }
+    private void Exit_OnClick(object sender, RoutedEventArgs e)
+    {
+        this.Close();
+    }
+    private void Menu_OnClick(object sender, RoutedEventArgs e)
+    {
+        MainWindow newWindow = new MainWindow();
+        newWindow.Show();
+        this.Close();
+    }
+
+    private void BackButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        MainWindow window = new MainWindow();
+        window.Show();
+        this.Close();
+    }
+}
---
+++ Ability-for-Duty-Clasification-System/ClassDefinition.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+
+namespace Ability_for_Duty_Clasification_System;
+
+public partial class ClassDefinition : Window
+{
+    public ClassDefinition()
+    {
+        InitializeComponent();
+    }
+    private void Exit_OnClick(object sender, RoutedEventArgs e)
+    {
+        this.Close();
+    }
+    private void Menu_OnClick(object sender, RoutedEventArgs e)
+    {
+        MainWindow newWindow = new MainWindow();
+        newWindow.Show();
+        this.Close();
+    }
+
+    private void ClassificateWindow_OnClick(object sender, RoutedEventArgs e)
+    {
+        ResultOfClassificateWindow window = new ResultOfClassificateWindow();
+        window.Show();
+        this.Close();
+    }
+
+    private void BackButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        MainWindow window = new MainWindow();
+        window.Show();
+        this.Close();
+    }
+}
